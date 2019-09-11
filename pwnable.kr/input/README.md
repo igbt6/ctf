02fbe23641d15651e1c5e815a1831a368f452e35
igbt6 <lukasz.uszko@gmail.com>
2019-09-11 20:49:59 +0200

    input done

--- pwnable.kr/input/README.md
+++ pwnable.kr/input/README.md
@@ -10,10 +10,10 @@ scp -P 2222 input2@pwnable.kr:~/* .
 The task seems to be easy. I have to create an input of size 100 bytes which contains valid data to pass all the checks.
 
 Script ```solver.py``` created.
+```scp -P2222 solver.py input2@pwnable.kr:~/```
 
 
-
-I got the flag:
+After running the script uned /tmp/luk6xff/ I got the flag:
 ```
-FLAG: {}
+FLAG: {Mommy! I learned how to pass various input in Linux :)}
 ```
--- pwnable.kr/input/solver.py
+++ pwnable.kr/input/solver.py
@@ -1,10 +1,10 @@
-
-
 import subprocess
 import os
 import sys
 import socket
 import time
+
+RUN_ON_SERVER = True
 
 # argv
 argv = ['A'] * 99
@@ -34,10 +34,15 @@ with open(b'\x0a', "wb") as f:
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # run process
-p = subprocess.Popen(["input2"]+argv, stdin=stdin_read, stderr=stderr_read, env=envs)
+if RUN_ON_SERVER:
+    # on pwnable.kr server you have to run the script from /tmp/ and create ln -s /home/input/flag flag 
+    p = subprocess.Popen(["/home/input2/input"]+argv, stdin=stdin_read, stderr=stderr_read, env=envs)
+    os.system("ln -s /home/input2/flag")
+else:
+    p = subprocess.Popen(["input"]+argv, stdin=stdin_read, stderr=stderr_read, env=envs)
 
 # network cont.
-time.sleep(1) # wait for server start
-s.connect(('localhost', port))
+time.sleep(3) # wait for server start
+s.connect(('127.0.0.1', port))
 s.send(b'\xde\xad\xbe\xef')
 s.close()
