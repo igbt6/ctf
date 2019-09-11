5ae33a2240382087b75252cadd9d533283053be9
Lukasz Uszko <lukasz.uszko@aptiv.com>
2019-09-11 17:57:08 +0200

    input update

--- pwnable.kr/input/README.md
+++ pwnable.kr/input/README.md
@@ -9,15 +9,11 @@ scp -P 2222 input2@pwnable.kr:~/* .
 2. Quick look at the source code of input.c
 The task seems to be easy. I have to create an input of size 100 bytes which contains valid data to pass all the checks.
 
-```
-
-$ python -c "print('A '*ord('A') + '\x00' + ' ' + '\x20 \x0a \x0d' + 'A '*(100-4-ord('A')))" > /tmp/payload
-$ ./input < /tmp/payload
-```
+Script ```solver.py``` created.
 
 
 
-I got get the flag:  
+I got the flag:
 ```
 FLAG: {}
 ```
--- pwnable.kr/input/solver.py
+++ pwnable.kr/input/solver.py
@@ -24,20 +24,20 @@ os.write(stdin_write, stdin_b)
 os.write(stderr_write, stderr_b)
 
 # env
-envs = {"\xde\xad\xbe\xef" : "\xca\xfe\xba\xbe"}
+envs = {b'\xde\xad\xbe\xef' : b'\xca\xfe\xba\xbe'}
 
 # file
-with open("\x0a", "wb") as f:
-    f.write(b"\x00\x00\x00\x00")
+with open(b'\x0a', "wb") as f:
+    f.write(b'\x00\x00\x00\x00')
 
 # network
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # run process
-p = subprocess.Popen(["input"]+argv, stdin=stdin_read, stderr=stderr_read, env=envs)
+p = subprocess.Popen(["input2"]+argv, stdin=stdin_read, stderr=stderr_read, env=envs)
 
 # network cont.
 time.sleep(1) # wait for server start
 s.connect(('localhost', port))
-s.send('\xde\xad\xbe\xef')
+s.send(b'\xde\xad\xbe\xef')
 s.close()
